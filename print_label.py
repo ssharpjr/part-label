12eea96ff945716a71aa4552f65cba1dc42f41cc
Stacey Sharp <ssharpjr@gmail.com>
2017-09-30 23:49:07 -0400

    Working but - The button release triggers a label.

--- print_label.py
+++ print_label.py
@@ -96,6 +96,7 @@ def main():
         try:
             # TODO: Wait for PLC trigger (button)
             # TODO: Need to test with RPI
+            # TODO: ERROR: The button release triggers a label!
             sleep(1)  # Add small delay before reading button
             btn_state = io.input(btn_pin)
             if btn_state == 1:  # Button is pushed
@@ -183,4 +184,4 @@ def test_run():
 
 
 if __name__ == '__main__':
-    test_run()
+    main()
