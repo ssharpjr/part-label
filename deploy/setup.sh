778e22ce1634808c6f1d14c83ec2c9da9d47a4c5
Stacey Sharp <ssharpjr@gmail.com>
2017-10-03 10:40:09 -0400

    Updated setup.sh to include PRESS_ID file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -7,18 +7,18 @@ APP_DIR="/home/${USER}/${APP}"
 
 PRESS_ID_FILE="/boot/PRESS_ID"
 DATE_FILE="${APP_DIR}/date_file.txt"
-SN_FILE="${APP_DIR}sn_file.txt"
+SN_FILE="${APP_DIR}/sn_file.txt"
 LOG_FILE="${APP_DIR}/log/serial_numbers.log"
 
 echo
 echo "Part Label Printer Setup Script"
 echo
+echo -n "Please enter the Press ID: "
+read PRESS_ID
+echo
 echo "Creating files..."
 echo
-echo "Assigning Press ID"
-echo
-echo "Please enter the Press ID"
-read PRESS_ID
+echo $PRESS_ID_FILE
 echo ${PRESS_ID} | sudo tee ${PRESS_ID_FILE} > /dev/null
 echo
 
