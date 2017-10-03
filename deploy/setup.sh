d29d55b3df8f3cbbd0ebcf6933789ad0c6e34bd8
Stacey Sharp <ssharpjr@gmail.com>
2017-10-03 10:20:56 -0400

    Updated setup.sh to include PRESS_ID file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ USER="pi"
 APP="part-label"
 APP_DIR="/home/${USER}/${APP}"
 
-
+PRESS_ID_FILE = "/boot/PRESS_ID"
 DATE_FILE="/tmp/date_file.txt"
 SN_FILE="/tmp/sn_file.txt"
 LOG_FILE="${APP_DIR}/log/serial_numbers.log"
@@ -14,6 +14,13 @@ echo
 echo "Part Label Printer Setup Script"
 echo
 echo "Creating files..."
+echo
+echo "Assigning Press ID"
+echo
+echo "Please enter the Press ID"
+read PRESS_ID
+echo ${PRESS_ID} | sudo tee ${PRESS_ID_FILE} > /dev/null
+echo
 
 echo $DATE_FILE
 echo "" > $DATE_FILE
